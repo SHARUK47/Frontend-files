b407092ab53a208cd53589a1bd1eacdcecad066d
SHARUK47 <sharukb47@gmail.com>
2022-07-14 19:51:08 +0530

    .

--- bootit/javascript/csa/f.js
+++ bootit/javascript/csa/f.js
@@ -2,5 +2,5 @@ let num1 = 300;
 let num2 = 400;
 let num3 = 500;
 Math.max()
-console.log(Math.max(num1,num2,num3))
-console.log(Math.min(num1,num2,num3))
+console.log("max value is", Math.max(num1,num2,num3))
+console.log("min value is", Math.min(num1,num2,num3))
