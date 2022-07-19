95c90fe4c232bce27cd6ab2c48db920fcc21a5a9
SHARUK47 <sharukb47@gmail.com>
2022-07-19 16:37:15 +0530

    hehe

---
+++ bootit/html files/meves.js
@@ -0,0 +1,9 @@
+function over(){document.getElementById("mv").style.backgroundColor = "red" ;}
+function out(){document.getElementById("mo").style.backgroundColor = "green" ;}
+function oc(){document.getElementById("mc").style.backgroundColor = "black" ;}
+function odc(){document.getElementById("mdc").style.backgroundColor = "violet" ;}
+function of(){document.getElementById("mf").style.backgroundColor = "yellow" ;}
+function ob(){
+    document.form1.type.value=document.form1.type.value.toUpperCase;
+}
+    
---
+++ bootit/javascript/classes/a.js
@@ -0,0 +1,10 @@
+class command {
+    Name = "vikram" ;
+    role = "agent" ;
+    kills = 22 ;
+}
+let a1 = new command () ;
+    
+ 
+console.log(new command ());
+console.log(a1.Name);
